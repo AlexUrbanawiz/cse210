925863ead6e9e60e625a7011291c9fb4f3d439f6
Sunpine522 <koalapokemonfun@gmail.com>
2024-10-31 09:48:18 -0600

    Testing cli use of git

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -8,6 +8,6 @@ class Program
         string first_name = Console.ReadLine();
         Console.Write("What is your last name? ");
         string last_name = Console.ReadLine();
-        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+        Console.WriteLine($"Your name is JOHN CENA!!! {last_name}, {first_name} {last_name}.");
     }
 }
