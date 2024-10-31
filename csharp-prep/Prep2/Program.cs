81a8188edf35624c61fc971f6ab2e6575df8e842
Sunpine522 <koalapokemonfun@gmail.com>
2024-10-31 09:36:30 -0600

    10/31 Prep 1-2 complete

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade(%) ");
+        int grade = int.Parse(Console.ReadLine());
+        string letter_grade;
+        if(grade < 60)
+        {
+            letter_grade = "F";
+        }
+        else if(grade < 80)
+        {
+            letter_grade = "C";
+        }
+        else if(grade< 90)
+        {
+            letter_grade = "B";
+        }
+        else
+        {
+            letter_grade = "A";
+        }
+
+        if(letter_grade != "F")
+        {
+            if(grade%10 <=3)
+            {
+                letter_grade = letter_grade + "-";
+            }
+            else if(letter_grade != "A" && grade%10 >=7)
+            {
+                letter_grade = letter_grade + "+";
+            }
+        }
+
+        if(grade >= 70)
+        {
+            Console.WriteLine("You passed! Congrats.");
+        }
+        else
+        {
+            Console.WriteLine("You didnt pass. Better luck next time.");
+        }
+
+        Console.WriteLine($"Your letter grade is {letter_grade}");
     }
 }
