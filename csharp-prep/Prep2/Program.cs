da50c1fd5d146add7f5700aaddaae7f49242777a
AlexUrbanawiz <koalapokemonfun@gmail.com>
2025-04-24 11:04:18 -0600

    Homework 1 Done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Homework 2
         Console.Write("What is your grade(%) ");
         int grade = int.Parse(Console.ReadLine());
         string letter_grade;
